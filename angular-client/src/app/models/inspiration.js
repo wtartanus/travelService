af3af75996e595b0f15fddc19085d977f4bf4e17
Wojtek Tartanus <wojtek.tartanus@airts.co.uk>
2017-08-07 20:51:54 +0100

    Removed unused files, and did some css for datepicker

--- angular-client/src/app/models/inspiration.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=inspiration.js.map
--- angular-client/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/modules/app.module.js';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
